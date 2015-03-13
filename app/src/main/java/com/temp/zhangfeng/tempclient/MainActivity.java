b7cf213f3848166b155198a05d6ecca9f3002dc4
ZhangFeng <zhangfenggo@gmail.com>
2015-03-13 19:51:43 +0800

    Initial commit

---
+++ app/src/main/java/com/temp/zhangfeng/tempclient/MainActivity.java
@@ -0,0 +1,66 @@
+package com.temp.zhangfeng.tempclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private TextView tempNum;//温度文本
+    private TextView targetTemp;//目标温度
+    private SeekBar sb;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tempNum = (TextView)findViewById(R.id.textView2);//获取温度的文本框
+        targetTemp = (TextView)findViewById(R.id.textView4);//获取目标温度
+
+        /*拖动条*/
+        sb = (SeekBar)findViewById(R.id.seekBar);
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                targetTemp.setText(Integer.toString(progress));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
