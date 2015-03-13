184672d8c53286ea432365c4474f7e82161a7857
ZhangFeng <zhangfenggo@gmail.com>
2015-03-13 21:23:54 +0800

    添加目标温度的设置

--- app/src/main/java/com/temp/zhangfeng/tempclient/MainActivity.java
+++ app/src/main/java/com/temp/zhangfeng/tempclient/MainActivity.java
@@ -2,10 +2,13 @@ package com.temp.zhangfeng.tempclient;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
         sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                targetTemp.setText(Integer.toString(progress));
+                targetTemp.setText(getTargetTemp(progress));
 
             }
 
@@ -42,25 +45,11 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    private String getTargetTemp(int progress) {
+        final int MaxTemp = 50;//最大值
+        final int MaxProgress = 100;
+        float f = (float)MaxTemp*progress/MaxProgress;//获取当前值
+        DecimalFormat df = new DecimalFormat("#0.0");//格式化数据 保留一位小数
+        return df.format(f);
     }
 }
